d702500618a91238269dc83585639d64a5124c06
Brian Mcclaugherty <brianmcclaugherty@gmail.com>
2021-11-28 23:49:15 -0500

    init

--- App.js
+++ App.js
@@ -1,14 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = ({
+      markers :[
+        {title: '1', coordinate: {latitude: 39.961178, longitude: -82.998795}},
+        {title: '2', coordinate: {latitude: 40.0992294, longitude: -83.1140771}}
+      ]
+  });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.map} 
+          initialRegion={{
+            latitude: 39.961178,
+            longitude: -82.998795,
+            latitudeDelta: 0.35,
+            longitudeDelta: 0.35}}>
+
+            {this.state.markers.map((marker ,i)=>(
+              <MapView.Marker
+                key={i}
+                coordinate={marker.coordinate}
+                title={'hello'}
+                description={"description"} />
+            ))}
+
+        </MapView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +47,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+}); 
+
+export default App;
